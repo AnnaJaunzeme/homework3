58f4658d64297c706ae70b481def89925075ed4d
Anna Jaunzeme <anna.jaunzeme@inbox.lv>
2021-10-14 15:23:12 +0300

    Add project files.

---
+++ homework3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                                  
+            Console.WriteLine("Enter your number here: ");
+              
+            
+            decimal number = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("The absolute value of your number is " + Math.Abs(number) + "!");
+            Console.ReadLine();
+
+           
+
+        }
+    }
+}
